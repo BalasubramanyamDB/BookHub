f53e12cb785b9b54336cc1047027012b60002873
BalasubramanyamDB <81681555+BalasubramanyamDB@users.noreply.github.com>
2023-05-22 18:42:58 +0530

    Update Landing.jsx

--- frontend/src/components/Landingpage/Landing.jsx
+++ frontend/src/components/Landingpage/Landing.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navhome from '../Navhome/Navhome';
-// import { useState,useEffect } from 'react';
-// import axios from 'axios';
+
 import '../Landingpage/Landing.css';
 
 const Landing = () => {
